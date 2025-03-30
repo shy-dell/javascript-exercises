d18420d87f09adfebcadefc4b74eb9838742eb72
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-30 19:31:23 -0400

    create removeFromArray to pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, position) {
+    const index = position - 1;
+    const spliced = array.toSpliced(index, 1);
+    return spliced;
 };
 
 // Do not edit below this line
