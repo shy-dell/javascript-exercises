742f17d0f163083ecc98dfb845535c38aa3e78f1
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-30 19:23:36 -0400

    Create reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let word = String(input);
+    let reverseWord = "";
+    for (let i = (word.length - 1); i >= 0; i--) {
+        reverseWord = reverseWord + word[i];
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
