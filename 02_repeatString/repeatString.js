3ae2d762838a898507d3dff1d00896c0b1516bb0
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-30 19:16:00 -0400

    Create repeatString function and remove skips from tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let words = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            words = words + string;
+        }
+    } else {
+        words = "ERROR";
+    }
+    return words
 };
+
+repeatString('hey',3)
 
 // Do not edit below this line
 module.exports = repeatString;
