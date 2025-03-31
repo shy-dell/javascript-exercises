646db0e616874037756980e231a6784d950c5169
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-30 20:52:19 -0400

    initial solution for test1 to sum all numbers within a range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+    let total = 0;
+    if (from >= 0 && to >= 0) {
+        for (let i = from; i <= to; i++) {
+            total = total + i;
+        }
+    } else {
+        console.log("please provide only positive numbers as inputs")
+    }
+    return total;
 };
 
 // Do not edit below this line
