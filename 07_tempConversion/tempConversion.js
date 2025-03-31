f1d9a772b598ce17e8a993ac5f5fc165ad55d58d
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-31 13:28:25 -0400

    create conversion funcs with ternary operator '?'

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+	const tempCelsius = ((tempFahrenheit - 32) * (5/9));
+	return tempCelsius === 0 ? tempCelsius : tempCelsius.toFixed(1);
+	
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+	const tempFahrenheit = (tempCelsius * (9/5) + 32);
+	return tempFahrenheit === 0 ? tempFahrenheit : tempFahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
