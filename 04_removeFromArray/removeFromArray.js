cfbb2e40deb771f23ccd6fa9a60a26e9eaf9fc01
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-30 20:45:41 -0400

    updated solution using filter method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,22 @@
+// const removeFromArray = function(array, ...toRemove) {
+//     // use Rest Parameters (...toRemove) to show optional number of inputs
+//     // Rest Params are Array instances and therefore use array methods
+//     for (let i = 0; i <= (array.length - 1); i++) {
+//         if (toRemove.includes(array[i])) {
+//             array.splice(i,1)
+//             i--
+//             // if the we splice the array, each element will not push forward 1 index bc the array is shorter
+//             // correct for this by decerementing before next increment
+//         } else {
+//             continue;
+//         }
+//     } return array;
+// } 
+
+// OPTION 2 with filter
 const removeFromArray = function(array, ...toRemove) {
-    // use Rest Parameters (...toRemove) to show optional number of inputs
-    // Rest Params are Array instances and therefore use array methods
-    for (let i = 0; i <= (array.length - 1); i++) {
-        if (toRemove.includes(array[i])) {
-            array.splice(i,1)
-            i--
-            // if the we splice the array, each element will not push forward 1 index bc the array is shorter
-            // correct for this by decerementing before next increment
-        } else {
-            continue;
-        }
-    } return array;
-} 
+    return array.filter((element) => !toRemove.includes(element));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
