1486bc1f6aa99ba21dc378bc2f39788391d3a646
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-31 10:25:11 -0400

    initial solution for test 1 and remove skips

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(date) {
+    if (Number.isInteger(date % 100)) {
+        if (Number.isInteger(date % 4) && Number.isInteger(date % 4)) {
+            return true;
+        }
+        else return false;
+    } else if (Number.isInteger(date % 4)){
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
