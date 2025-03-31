67cfd546c25952a6f94ed433df32b1eb68f821a6
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-31 13:33:34 -0400

    alternative solution using math.round that abbreviates to one line

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,11 @@
 const convertToCelsius = function(tempFahrenheit) {
-	const tempCelsius = ((tempFahrenheit - 32) * (5/9));
-	return tempCelsius === 0 ? tempCelsius : Number(tempCelsius.toFixed(1));
+	return Math.round((tempFahrenheit - 32) * (5/9) * 10) / 10;
+	
 	
 };
 
 const convertToFahrenheit = function(tempCelsius) {
-	const tempFahrenheit = (tempCelsius * (9/5) + 32);
-	return tempFahrenheit === 0 ? tempFahrenheit : Number(tempFahrenheit.toFixed(1));
+	return Math.round((tempCelsius * (9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
