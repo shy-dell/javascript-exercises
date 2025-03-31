5b6ad8c7e1cf3ed1ff2d1c3ada0f7481fa877d34
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-31 13:07:54 -0400

    update function to use division rather than modulo

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,7 @@
 const leapYears = function(date) {
-    if (Number.isInteger(date % 100)) {
-        if (Number.isInteger(date % 4) && Number.isInteger(date % 4)) {
-            return true;
-        }
-        else return false;
-    } else if (Number.isInteger(date % 4)){
+    if (Number.isInteger(date / 100) && Number.isInteger(date / 400)) {
+        return true;
+    } else if (Number.isInteger(date / 4) && !Number.isInteger(date / 100)){
         return true;
     }
     else return false;
