b7da58ce5e7546fbc3ae913a420447ba6dceffa0
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-31 13:31:16 -0400

    fix: toFixed returns string so use Number or parseFloat

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
 const convertToCelsius = function(tempFahrenheit) {
 	const tempCelsius = ((tempFahrenheit - 32) * (5/9));
-	return tempCelsius === 0 ? tempCelsius : tempCelsius.toFixed(1);
+	return tempCelsius === 0 ? tempCelsius : Number(tempCelsius.toFixed(1));
 	
 };
 
 const convertToFahrenheit = function(tempCelsius) {
 	const tempFahrenheit = (tempCelsius * (9/5) + 32);
-	return tempFahrenheit === 0 ? tempFahrenheit : tempFahrenheit.toFixed(1);
+	return tempFahrenheit === 0 ? tempFahrenheit : Number(tempFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
