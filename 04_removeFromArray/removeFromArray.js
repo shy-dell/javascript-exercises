bae4bc35eff9d1c80ea4ffdeca93873da82f5e68
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-30 20:40:10 -0400

    remove test skips and create function to handle multiple inputs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,17 @@
-const removeFromArray = function(array, position) {
-    const index = position - 1;
-    const spliced = array.toSpliced(index, 1);
-    return spliced;
-};
+const removeFromArray = function(array, ...toRemove) {
+    // use Rest Parameters (...toRemove) to show optional number of inputs
+    // Rest Params are Array instances and therefore use array methods
+    for (let i = 0; i <= (array.length - 1); i++) {
+        if (toRemove.includes(array[i])) {
+            array.splice(i,1)
+            i--
+            // if the we splice the array, each element will not push forward 1 index bc the array is shorter
+            // correct for this by decerementing before next increment
+        } else {
+            continue;
+        }
+    } return array;
+} 
 
 // Do not edit below this line
 module.exports = removeFromArray;
