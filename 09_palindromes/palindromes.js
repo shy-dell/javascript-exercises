aa8714e5efd2e494a59e2bf831fdbc1b96dc2e16
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-04-16 09:17:18 -0400

    Solutions to palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (forwards) {
+    const alphaNum = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let arr = [...forwards
+        .toLowerCase()
+        .split(' ')
+        .join(',')]
+        .filter(letter => alphaNum.includes(letter))
+    let backwards = [...arr].reverse();
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === backwards[i]) {
+            continue
+        } else {
+            return false
+        };
+    } return true;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
