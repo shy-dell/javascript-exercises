fdcf662de9332b31f946070667bf77ddba6c8dc1
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-04-15 21:29:12 -0400

    Solve calculator and all tests pass

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
+const add = function(a,b) {
+    return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+    return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+    return arr.reduce((total, component) => total = total + component, 0);
 	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    return arr.reduce((product, factor) => product = product * factor, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	arr = [];
+    for (let i = num; i > 0; i--) {
+        arr.push(i)
+    }
+    return arr.reduce((product, factor) => product = product * factor, 1);
 };
 
 // Do not edit below this line
