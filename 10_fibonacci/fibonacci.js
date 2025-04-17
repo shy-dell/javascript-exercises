d8b45891ac64d6c0cf5b45709af6c679b36649a0
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-04-17 19:28:33 -0400

    Initial fibonnaci loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    Number(number);
+    let prevNumber = 0;
+    let arr = [1];
+    if (number < 0) {
+        return "Oops"
+    } else if (number === 0) {
+        return prevNumber;
+    } else {
+        for (let i = 0; i < number - 1; i++){
+            let nextNumber = prevNumber + arr[i];
+            arr.push(nextNumber);
+            prevNumber = arr[i];
+        }
+    }
+    return arr.pop();
 };
 
 // Do not edit below this line
