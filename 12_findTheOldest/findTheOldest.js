24f7e196b8a263d7bff5a0b352ca4ffa0ccf977b
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-04-17 21:51:51 -0400

    Find the oldest person and remove skips on test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let newPeople = people.map((person) => {
+        if (person.yearOfDeath !== undefined) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = currentYear - person.yearOfBirth;
+        }
+        return person;
+    });
+    newPeople.sort((a,b) => b.age - a.age);
+    return newPeople[0];
 };
 
 // Do not edit below this line
